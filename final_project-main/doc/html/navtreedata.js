5128ec5738f84982e5f5e445e817c60ed7ee0cf3
jpittma1 <91224170+jpittma1@users.noreply.github.com>
2021-12-08 10:49:27 -0500

    Add files via upload

---
+++ final_project-main/doc/html/navtreedata.js
@@ -0,0 +1,31 @@
+var NAVTREE =
+[
+  [ "809Y Final Project (Novak, Pittman, Smith)", "index.html", [
+    [ "final_project", "md__r_e_a_d_m_e.html", null ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
