feae6ffbddb731f3713849ee09e2bf8fe3f7b6f1
njnovak <nnovak@umd.edu>
2021-12-09 14:58:32 -0500

    explorer

--- final_project-main/src/explorer.cpp
+++ final_project-main/src/explorer.cpp
@@ -18,10 +18,27 @@ Explorer::Explorer(ros::NodeHandle* nodehandle const std::string& robot_name) : 
 {
     start_place = Explorer::get_start_loc();
 
-    void get_goal(){
+    void get_goals(){
         exp_node.parse(aruco_lookup.yaml,"yaml");
         std::cout << exp_node["target 1"].as_float64() << std::endl;
+        goal_list[0][0] = exp_node["target 1"][0].as_float64();
+        goal_list[0][1] = exp_node["target 1"][1].as_float64();
+        goal_list[1][0] = exp_node["target 2"][0].as_float64();
+        goal_list[0][1] = exp_node["target 2"][1].as_float64();
+        goal_list[1][0] = exp_node["target 3"][0].as_float64();
+        goal_list[0][1] = exp_node["target 3"][1].as_float64();
+        goal_list[1][0] = exp_node["target 4"][0].as_float64();
+        goal_list[0][1] = exp_node["target "][1].as_float64();
+
+
     }
     
 
+
+}
+
+int main(){
+    exp_node.parse(aruco_lookup.yaml,"yaml");
+    std::cout << exp_node["target 1"].as_float64() << std::endl;
+    
 }
