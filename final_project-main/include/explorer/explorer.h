4f0b188f20f30b989bc76e665a89eb062c0abd17
JERRY PITTMAN <jerry.pittmanjr@gmail.com>
2021-12-12 14:11:10 -0500

    brackets

--- final_project-main/include/explorer/explorer.h
+++ final_project-main/include/explorer/explorer.h
@@ -2,7 +2,7 @@
 #define EPLORER_H
 
 #include "../include/bot_controller/bot_controller.h"
-#include "bot_msgs/BotStatus.h"
+#include <bot_msgs/BotStatus.h>
 #include <geometry_msgs/Twist.h>   //for geometry_msgs::Twist
 #include <nav_msgs/Odometry.h>     //for nav_msgs::Odometry
 #include <sensor_msgs/LaserScan.h> //for laser scans
